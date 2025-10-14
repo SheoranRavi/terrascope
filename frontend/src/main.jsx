7192a1dcbeae053e2c14e959dcaa73f3618eb7d9
Ravi Sheoran <ravisheoranr@gmail.com>
2025-10-14 19:51:36 +0530

    icon and markers fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,18 @@ import { Provider } from './components/ui/provider'
 import './index.css'
 import App from './App.jsx'
 import { SearchProvider } from './context/SearchContext.jsx'
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 // This is to get around the leaflet-draw type bug
 window.type = '';
